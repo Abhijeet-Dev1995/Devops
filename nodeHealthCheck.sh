2bfafecbe7810ee76f96da82d2646b942155e292
Abhijeet <abhijeet_dev@outlook.com>
2024-04-13 00:50:27 +0530

    ShellScriptUpdated

--- nodeHealthCheck.sh
+++ nodeHealthCheck.sh
@@ -9,6 +9,13 @@
 # Version v1
 #############################
 
+set -x	# debug mode
+set -e	# exit the script when there is an error
+set -o pipefail	# set -e bydefault doesn't catch ur pipefailure, so need to use set -o
+
+dasdafsfasf | echo		//set -e will treat this line as valid by seeing by last pipe argument
+dsfafsfaasf | echo | afsfa 	//set -e will treat this line as failure
+afafafasffa | echo | adad | echo //set -e will treat this line as valid by seeing by last pipe argument
 
 df -h
 
