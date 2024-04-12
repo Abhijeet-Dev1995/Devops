9afc430a1ff538871c8d57b5cab5016bc9efdb6c
Abhijeet <abhijeet_dev@outlook.com>
2024-04-13 00:41:28 +0530

    ShellScriptUpdated

---
+++ nodeHealthCheck.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#############################
+# Author : Abhijeet
+# Date : 13/04/2024
+#
+# This script outputs the node health
+#
+# Version v1
+#############################
+
+
+df -h
+
+free -g
+
+nproc
